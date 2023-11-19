e0a6f2501751fcfde38c0ff9cb75dc58973ab305
shawn nest <127818953+zhendegaoshou@users.noreply.github.com>
2023-11-19 15:45:27 +0800

    Update PhotoGalleryActivity.kt

--- PhotoGallery/app/src/main/java/com/example/photogallery/PhotoGalleryActivity.kt
+++ PhotoGallery/app/src/main/java/com/example/photogallery/PhotoGalleryActivity.kt
@@ -9,9 +9,10 @@ class PhotoGalleryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_photo_gallery)
+        // 从这个地方可以获取你的存储文件地址
         val file_path = getExternalFilesDir(null)?.absolutePath+ File.separator+"photo"
-
         Log.d(TAG,file_path)
+        
         val isFragmentContainerEmpty = savedInstanceState == null
         if(isFragmentContainerEmpty)
         {
